dff3450445019b4a6095115bb9e6901416a25d2b
Michael Hutchinson <mhutchin@hemispingus.stats.ox.ac.uk>
2019-11-13 13:07:20 +0000

    Added MLP encoders and decoders, and latent encoders

---
+++ src/encoder.py
@@ -0,0 +1,99 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+from src.utils import MLP
+
+
+class DeterministicMLPEncoder(nn.Module):
+    """ Maps (x_i, y_i) pairs to a representation vector r_i using a
+    MLP network.
+
+    Parameters
+    ----------
+    x_dim : int 
+        Dimension of the x values
+    
+    y_dim : int
+        Dimension of the y values
+
+    r_dim : int
+        The dimension of the representation vector
+
+    hid_dim : int
+        Dimension of the hidden layers
+
+    num_hid : int
+        The number of hidden layers to use
+    """
+
+    def __init__(self, x_dim, y_dim, r_dim, hid_dim=50, num_hid=3):
+        super(DeterministicMLPEncoder, self).__init__()
+
+        self.x_dim = x_dim
+        self.y_dim = y_dim
+        self.r_dim = r_dim
+        self.hid_dim = hid_dim
+        self.num_hid = num_hid
+
+        self.model = MLP(self.x_dim + self.y_dim, self.r_dim, self.hid_dim, self.num_hid, nn.ReLU())
+
+    def forward(self, x, y):
+        """ passes a batch of context x and y throught the encoder 
+        to get a represnetation vector.
+
+        x : torch.Tensor
+            Shape (batch_size, x_dim)
+
+        y : torch.Tensor
+            Shape (batch_size, y_dim)
+
+        return : torch.Tensor
+            Shape (batch_size, r_dim) 
+
+        """
+        input = torch.cat((x, y), dim=1)
+        return self.model(input)
+
+
+class LatentMLPEncoder(nn.Module):
+    """ Maps a represnetation vector r to a Gaussian 
+    distributed latent variable.
+
+    Parameters
+    ----------
+    r_dim : int
+        The dimension of the representation vector
+
+    z_dim : int
+        The dimension of the latent variable z
+    """
+
+    def __init__(self, r_dim, z_dim):
+        super(LatentMLPEncoder, self).__init__()
+        self.r_dim = r_dim
+        self.z_dim = z_dim
+
+        hidden_dim = int( (r_dim + z_dim) /2 ) # from the tf code - not clear why this is chosen
+
+        self.r_to_hidden = nn.Linear(r_dim, hidden_dim)
+        self.hidden_to_mu = nn.Linear(hidden_dim, z_dim)
+        self.hidden_to_pre_sigma = nn.Linear(hidden_dim, z_dim)
+
+    def forward(self, r):
+        """
+        r : torch.Tensor
+            Shape (batch_size, r_dim)
+
+        return : torch.Tensor
+            Shape (batch_size, z_dim)
+        """
+        hidden = torch.relu(self.r_to_hidden(r))
+        mu = self.hidden_to_mu(hidden)
+        pre_sigma = self.hidden_to_pre_sigma(hidden)
+
+        # formulation from "Attentive Neral Processes" and "Empirical Evaluation of Neural Process Objectives"
+        # Seems silly to call it log sigma in this case. Might think about switching to an actual log encoding.
+        sigma = 0.1 + 0.9 * F.softplus(pre_sigma)
+
+
---
+++ src/utils.py
@@ -0,0 +1,46 @@
+import torch
+from torch import nn
+
+
+class MLP(nn.Module):
+    """ Defines a basic MLP network.
+
+    Parameters
+    ----------
+    in_dim : int 
+        Dimension of the input
+
+    out_dim : int
+        The dimension of the output
+
+    hid_dim : int
+        Dimension of the hidden layers
+
+    num_hid : int
+        The number of hidden layers to use
+
+    activation : torch.nn.Module
+        The activation function to use between layers
+    """
+
+    def __init__(self, in_dim, out_dim, hid_dim, num_hid, activation=nn.ReLU):
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.hid_dim = hid_dim
+        self.num_hid = num_hid
+        self.activation = activation
+
+        layers = []
+        sizes = [self.in_dim] + [self.hid_dim] * num_hid + [self.out_dim]
+
+        # append layers with acitvation functions
+        for i in range(len(sizes) - 2):
+            layers.append(nn.Linear(sizes[i], sizes[i+1]))
+
+        # append final layer with no activations
+        layers.append(nn.Linear(sizes[-2], sizes[-1]))
+
+        self.model = nn.Sequential(layers)
+
+    def forward(self, input):
+        return self.model(input)
