92fcdeb272b85328f89b86e23ff906941dfe07a2
Harrison Zhu <harrisonzhu5080@gmail.com>
2019-11-19 17:47:04 +0000

    add 2d experiments

--- test_train.py
+++ test_train.py
@@ -1,13 +1,13 @@
-from src.datagen.gpcurve import MaternGPCurvesReader
+from src.datagen.gpcurve import MaternGPCurvesReader, ProductRBFCurvesReader
 from src.decoder import HeteroskedasticDecoder
 from src.encoder import LatentEncoder, DeterministicEncoder
 from src.aggregator import Attention
 from src.neural_process import AttentiveNeuralProcess
 from torch import optim
-from src.train import train
+from src.train import train, train_spatiotemporal
 
 MAX_CONTEXT_POINTS = 16
-random_kernel_parameters = True
+random_kernel_parameters = False
 BATCH_SIZE = 16
 hyperparameters = {
     "EPOCHS" : 10000,
@@ -16,7 +16,7 @@ hyperparameters = {
 # Sizes of the layers of the MLPs for the encoders and decoder
 # The final output layer of the decoder outputs two values, one for the mean and
 # one for the variance of the prediction at the target location
-x_dim = 1
+x_dim = 2
 y_dim = 1
 r_dim = 64
 z_dim = 64
@@ -29,14 +29,11 @@ att = Attention('mlp','multihead', x_dim, r_dim, hid_dim=hid_dim, num_hid=num_hi
 model = AttentiveNeuralProcess(detencoder, att, latencoder, decoder, True)
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-datagen = MaternGPCurvesReader(
-    batch_size=BATCH_SIZE, max_num_context=MAX_CONTEXT_POINTS, random_kernel_parameters=random_kernel_parameters
-)
+datagen = ProductRBFCurvesReader(BATCH_SIZE, MAX_CONTEXT_POINTS, random_kernel_parameters=random_kernel_parameters)
 
 # Test dataset
-datagen_test = MaternGPCurvesReader(
-    batch_size=1, max_num_context=MAX_CONTEXT_POINTS, testing=True, random_kernel_parameters=random_kernel_parameters)
+datagen_test = ProductRBFCurvesReader(batch_size=1, max_num_context=MAX_CONTEXT_POINTS, testing=True, random_kernel_parameters=random_kernel_parameters)
 
-y_target_mu, y_target_sigma, log_pred, kl_target_context, loss = train(model, hyperparameters, datagen, datagen_test, optimizer, save=True, experiment_name='anp_matern_gp')
+y_target_mu, y_target_sigma, log_pred, kl_target_context, loss = train_spatiotemporal(model, hyperparameters, datagen, datagen_test, optimizer, save=True, experiment_name='anp_matern_gp')
 
 print('FINISHED')
