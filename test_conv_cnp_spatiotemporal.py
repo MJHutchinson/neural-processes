5176da32fb76cb49aa29028b3927bf742a6a6676
Harrison Zhu <harrisonzhu5080@gmail.com>
2019-11-21 16:31:42 +0000

    add spatiotemoral experiment setup

--- test_conv_cnp_spatiotemporal.py
+++ test_conv_cnp_spatiotemporal.py
@@ -4,9 +4,9 @@ import torch
 import torch.optim as optim
 import matplotlib.pyplot as plt
 
-from src.conv_cnp import ConvCNP_multidim
-from src.datagen.gpcurve import *
-from src.utils import plot_functions
+from src.conv_cnp import ConvCNP
+from src.datagen.product_gpcurve import *
+from src.utils import plot_function
 
 torch.manual_seed(0)
 
@@ -19,7 +19,7 @@ datagen = ProductRBFCurvesReader(16, MAX_CONTEXT_POINTS, random_kernel_parameter
 datagen_test = ProductRBFCurvesReader(batch_size=1, max_num_context=MAX_CONTEXT_POINTS, testing=True, random_kernel_parameters=random_kernel_parameters)
 
 
-conv_cnp = ConvCNP_multidim(x_dim=2, y_dim=1)
+conv_cnp = ConvCNP(2, 1).cuda()
 
 optimizer = optim.Adam(conv_cnp.parameters(), lr=0.001)
 
@@ -29,23 +29,29 @@ for epoch in range(EPOCHS):
     epoch_loss = 0
     # Train dataset
     data_train, _ = datagen.generate_curves()
-    x_context = data_train.query[0][0].contiguous().transpose(1,2)
-    y_context = data_train.query[0][1].contiguous().transpose(1,2)
-    x_target = data_train.query[1].contiguous().transpose(1,2)
-    y_target = data_train.target_y.contiguous().transpose(1,2)
+    x_context = data_train.query[0][0].contiguous().cuda()
+    y_context = data_train.query[0][1].contiguous().cuda()
+    x_target = data_train.query[1].contiguous().cuda()
+    y_target = data_train.target_y.contiguous().cuda()
     optimizer.zero_grad()
     
-    y_target_mu, y_target_sigma, loss = conv_cnp.forward(x_context, y_context, x_target, y_target)
+    y_pred_target_mu, y_pred_target_sigma, loss, _, _ = conv_cnp.forward(x_context, y_context, x_target, y_target)
     loss.sum().backward()
     optimizer.step()
     
     if (epoch % 500) == 0:
+        print(f'epoch: {epoch}, loss: {loss.sum()}, x_kernel_length: {conv_cnp.kernel_x.length_scale:0.4f}, rho_kernel_length: {conv_cnp.kernel_rho.length_scale:0.4f}')
         data_test, target_sigma = datagen_test.generate_curves()
-        x_context = data_test.query[0][0].contiguous().transpose(1,2)
-        y_context = data_test.query[0][1].contiguous().transpose(1,2)
-        x_target = data_test.query[1].contiguous().transpose(1,2)
-        y_target = data_test.target_y.contiguous().transpose(1,2)
-        y_target_mu, y_target_sigma, _ = conv_cnp.forward(x_context, y_context, x_target, y_target)
+        x_context = data_test.query[0][0].contiguous().cuda()
+        y_context = data_test.query[0][1].contiguous().cuda()
+        x_target = data_test.query[1].contiguous().cuda()
+        y_target = data_test.target_y.contiguous().cuda()
+        y_pred_target_mu, y_pred_target_sigma, loss, _, _ = conv_cnp.forward(x_context, y_context, x_target, y_target)
+
+        x_target =  x_target.cpu()
+        target_sigma = target_sigma.cpu()
+        y_pred_target_mu = y_pred_target_mu.cpu()
+        y_pred_target_sigma = y_pred_target_sigma.cpu()
 
         plt.figure()
         plt.scatter(
@@ -73,7 +79,7 @@ for epoch in range(EPOCHS):
         plt.scatter(
             x_target[0, :, 0].squeeze(-1),
             x_target[0, :, 1].squeeze(-1),
-            c=y_target_mu[0].squeeze(-1).data,
+            c=y_pred_target_mu[0].squeeze(-1).data,
             marker="s",
         )
         plt.colorbar()
@@ -83,12 +89,9 @@ for epoch in range(EPOCHS):
         plt.scatter(
             x_target[0, :, 0].squeeze(-1),
             x_target[0, :, 1].squeeze(-1),
-            c=y_target_sigma[0].squeeze(-1).data,
+            c=y_pred_target_sigma[0].squeeze(-1).data,
             marker="s",
         )
         plt.colorbar()
         plt.savefig("results/spatial_convnp/pred_sigma_{}.png".format(epoch))
         
-        print(
-            f"Iter: {epoch}, loss: {loss.sum()}"
-        )
